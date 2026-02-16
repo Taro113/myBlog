c9481e74f5b09220b2c0a517d3b591ba2ec28115
郜健宇 <18538370571@163.com>
2026-02-16 14:54:07 +0800

    更新脚本

--- start.bat
+++ start.bat
@@ -3,23 +3,24 @@ REM 切换到 main 分支
 git checkout main
 
 REM 编译
-call npm install
-call npm run docs:build
+npm install
+npm run docs:build
 
 REM 提交编译文件到 main 分支
 git add .
 git commit -m "编译上传"
 git push origin main
 
-REM 切换到 dist 分支（如果不存在则创建）
-git checkout dist 2>nul || git checkout -b dist
+REM 切换到 dist 分支
+git checkout dist
+git pull
 
 REM 从 main 分支检出编译后的静态文件
 git checkout origin/main -- docs/.vitepress/dist
 
 REM 用 robocopy 将文件同步到项目根目录
 robocopy .\docs\.vitepress\dist\ .\ /E
-rmdir /s /q ".\docs\.vitepress\dist"
+@REM rmdir /s /q ".\docs\.vitepress\dist"
 
 REM 提交并推送到 dist 分支
 git add .
